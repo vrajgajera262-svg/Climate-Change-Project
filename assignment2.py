a93bb461d760b1d2b08c6a2d23cfe8ecc628d5e6
Gajera Vraj <vrajgajera262@gmail.com>
2026-03-29 20:26:31 +0100

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+"""
+4PAM2024 Data Science Lab 1 - Assignment 2
+Author  : Vraj Gajera
+Date    : 24/03/2026
+
+Title   : Statistical Analysis of CO2 Emissions and Socioeconomic Indicators
+
+Description:
+This program analyses relationships between CO2 emissions and
+key development indicators (GDP, renewable energy, urbanisation)
+across countries of different income levels.
+
+The analysis includes:
+- Data cleaning (World Bank format)
+- Descriptive statistics
+- Correlation analysis
+- Temporal comparison (early vs recent periods)
+- Rate of change estimation
+- Bootstrapping for statistical robustness
+- Visualisation
+"""
+
